1a8ee64a6fcb9af2101282ae193e5b9507321a64
Alex Cardenas <alexandercardenas50x@gmail.com>
2026-02-28 11:29:35 -0500

    feet: Modificando la informacion de las variables , Fase 1 terminada , modificada por Alexander

--- nivel_1_variables.js
+++ nivel_1_variables.js
@@ -1,8 +1,8 @@
 
-let heroe="loki";
-let poder="magia";
-let nivelEnergia=120;
-let ciudad="asgard";
-let enemigo="khan";
+let heroe = "superman";
+let poder = "superfuerza";
+let nivelEnergia = 120;
+let ciudad = "Ghotam";
+let enemigo = "lex luthor";
 
-console.log(heroe,"se enfrenta a ",enemigo,"en la ciudad de ",ciudad,"donde su poder de",poder,"es de",nivelEnergia)
+console.log(heroe, "se enfrenta a ", enemigo, "en la ciudad de ", ciudad, "donde su poder de", poder, "es de", nivelEnergia)
