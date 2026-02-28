290ed3c2a62bb3049b099c2a876ef693b75ec948
Alex Cardenas <alexandercardenas50x@gmail.com>
2026-02-28 09:44:57 -0500

    heroe 1 creado

---
+++ nivel_1_variables.js
@@ -0,0 +1,8 @@
+nombreHeroe = "Batman"
+poder = "Dinero"
+nivelEnergia = 80
+ciudad = "Ghotam"
+enemigo = "joker"
+
+console.log("El superheroe llamado ",nombreHeroe,"se enfrento a su enemigo ",enemigo," ,en la ciudad de ",ciudad)
+
