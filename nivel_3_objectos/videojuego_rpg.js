1e4c64bcfaf34cd8453a7ba590b59f0444ef9e48
Alex Cardenas <alexandercardenas50x@gmail.com>
2026-03-02 11:17:35 -0500

    feet: videojuego_rpg , creacion de objectos jugador y enemigo

---
+++ nivel_3_objectos/videojuego_rpg.js
@@ -0,0 +1,15 @@
+let jugador = {
+    nombre: "Alex",
+    vida: 100,
+    ataque: 20,
+    experiencia: 0,
+    nivel: 1
+}
+let enemigo = {
+    nombre: "evil",
+    vida: 100,
+    ataque: 10,
+    experiencia: 0,
+    nivel: 1
+}
+
