0d1e56a961c01fc95c0dee1b5270778b761ea089
luis enrique <luiscajacurigutierrezgmail.com>
2026-03-02 11:30:31 -0500

    agregue funcion ataque que modifica la vida, funcion maxfuerza para aumentar la fuerza,
    y funcion para subir de nivel

--- nivel_3_objetos/videojuego.js
+++ nivel_3_objetos/videojuego.js
@@ -13,10 +13,45 @@ let jugador2={
 }
 
 function mostrarEstado(jugador){
+    console.log("ESTADO DEL JUGADOR",jugador.nombre,"***********************")
     console.log(jugador.nombre,"tiene",jugador.vida,"puntos de vida")
     console.log(jugador.nombre,"tiene",jugador.fuerza,"puntos de fuerza")
     console.log(jugador.nombre,"tiene",jugador.nivel,"puntos de nivel")
     console.log("#######################")
 }
+mostrarEstado(jugador)
+mostrarEstado(jugador2);
+
+function ataque(atacante,atacado){
+    atacado.vida=atacado.vida-atacante.fuerza;
+    console.log(atacante.nombre,"ataca a",atacado.nombre)
+    console.log(atacado.nombre,"tiene",atacado.vida,"puntos de vida")
+    console.log("#######################")
+}
+
+
+function maxfuerza(jugador){
+    console.log(jugador.nombre,"tiene",jugador.fuerza,"puntos de fuerza")
+    jugador.fuerza += 25
+    console.log(jugador.nombre,"usa max fuerza y aumenta su fuerza a",jugador.fuerza,"puntos de fuerza")
+    console.log("#######################")
+
+}
+
+
+
+function nivelA(jugador){
+    console.log(jugador.nombre,"tiene",jugador.nivel,"puntos de nivel")
+    jugador.nivel += 1
+    console.log(jugador.nombre,"sube de nivel a",jugador.nivel)
+    console.log("#######################")
+}
+
+ataque(jugador,jugador2);
+maxfuerza(jugador2);
+ataque(jugador2,jugador);
+nivelA(jugador);
+
+
 mostrarEstado(jugador);
 mostrarEstado(jugador2);
