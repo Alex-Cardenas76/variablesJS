05c27c9a0af0fe036619a31efdaf65c7d39a1769
Alex Cardenas <alexandercardenas50x@gmail.com>
2026-03-02 11:12:32 -0500

    Creacion de las funciones ordenarComida(), pagar() , mostrarEstado(), funciones correctamente probadas

--- nivel_3_objectos/simulador_restaurante.js
+++ nivel_3_objectos/simulador_restaurante.js
@@ -3,5 +3,29 @@ let cliente = {
     dinero: 50,
     hambre: 80
 };
+function ordenarComida(sujeto) {
+    sujeto.hambre = sujeto.hambre - 20
+    console.log("El cliente ha pedido una amburguesa")
+    console.log("El cliente consume la amburguesa: ")
+    console.log("nivel de hambre: ", sujeto.hambre)
+    console.log("*********************************")
+}
 
+function pagar(sujeto) {
+    sujeto.dinero -= 10
+    console.log("El cliente paga la hamburguesa")
+    console.log("Dinero restante: S/.", sujeto.dinero)
+    console.log("*********************************")
+}
+
+function mostrarEstado(sujeto) {
+    console.log("Nombre: ", sujeto.nombre)
+    console.log("Dinero: S/.", sujeto.dinero)
+    console.log("Nivel de hambre: ", sujeto.hambre)
+    console.log("*********************************")
+}
+ordenarComida(cliente)
+ordenarComida(cliente)
+pagar(cliente)
+mostrarEstado(cliente)
 
