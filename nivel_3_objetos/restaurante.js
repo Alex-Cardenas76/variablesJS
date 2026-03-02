00e6f1ea0a7e1d9ed6894cef2eb06edad5ce004f
luis enrique <luiscajacurigutierrezgmail.com>
2026-03-02 11:08:00 -0500

    agregando funcion para ordenarcomida y pagar

--- nivel_3_objetos/restaurante.js
+++ nivel_3_objetos/restaurante.js
@@ -15,4 +15,23 @@ function mostrarEstado(cliente){
 }
 mostrarEstado(cliente);
 
+function ordenarComida(cliente){
+    cliente.hambre=cliente.hambre-50;
+    console.log(cliente.nombre,"ordeno un chaufa de pollo")
+    cuenta=cuenta+12;
+    console.log("cuenta S/"+cuenta)
+    console.log("#######################")
+}
+ordenarComida(cliente);
 
+
+function pagar(cliente){
+    cliente.dinero=cliente.dinero-cuenta;
+    console.log(cliente.nombre,"recibe la cuenta de",cuenta,"soles")
+    console.log(cliente.nombre,"paga la cuenta de su comida")
+    console.log("#######################")
+}
+pagar(cliente);
+
+
+mostrarEstado(cliente);
